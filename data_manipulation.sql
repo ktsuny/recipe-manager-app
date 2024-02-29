080a233326fe595c17e9de4e2b6f459d10416b21
ktsuny <ktsuny@berkeley.edu>
2024-02-29 11:36:51 -0800

    converting to flask app

--- data_manipulation.sql
+++ data_manipulation.sql
@@ -4,17 +4,15 @@ DROP TABLE IF EXISTS Recipes;
 
 CREATE table Recipes (
 	id int AUTO_INCREMENT NOT NULL,
-    name varchar(50),
+    recipe_name varchar(50) NOT NULL,
     serving_size int NOT NULL DEFAULT 2,
-    ingredients varchar(100) NOT NULL,
-    steps varchar(500)
+    ingredients varchar(300) NOT NULL,
+    steps varchar(700),
+    PRIMARY KEY (id)
 );
 
-INSERT INTO Recipes (serving_size, ingredients, steps)
-VALUES(2, 'tomato, egg, salt', '1. add oil 2. whisk eggs in a bowl with a pinch of salt 3. stir fry eggs when pan is hot 4. put eggs aside 5. stir fry tomatoes until soft and juicy 6. add in eggs and cook for 10 seconds');CREATE table Recipes (
-    serving_size int NOT NULL DEFAULT 2,
-    ingredients varchar(100) NOT NULL,
-    steps varchar(500)
-);
+INSERT INTO Recipes (recipe_name, serving_size, ingredients, steps)
+VALUES('tomato egg', 2, 'tomato, egg, salt', '1. add oil 2. whisk eggs in a bowl with a pinch of salt 3. stir fry eggs when pan is hot 4. put eggs aside 5. stir fry tomatoes until soft and juicy 6. add in eggs and cook for 10 seconds');
+
 
 SELECT * from Recipes;
