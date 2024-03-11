ba2392c285ce8faec1a992bc96b29736e2fc193e
Yushu Sun <sunyus@flip2.engr.oregonstate.edu>
2024-03-11 14:57:26 -0700

    still working on calling microservice

--- app.py
+++ app.py
@@ -76,6 +76,22 @@ def recipe_detail(recipe_id):
         # render edit_cats page passing our query data and adopter data to the edit_cats template
         return render_template("search_results.j2", data=data)
 
+@app.route('/number', methods=['GET'])
+def get_products():
+    response = requests.get(f"{BASE_URL}/products")
+    if response.status_code != 200:
+        return jsonify({'error': response.json()['message']}), response.status_code
+    products = []
+    for product in response.json()['products']:
+        product_data = {
+            'id': product['id'],
+            'title': product['title'],
+            'brand': product['brand'],
+            'price': product['price'],
+            'description': product['description']
+        }
+        products.append(product_data)
+    return jsonify({'data': products}), 200 if products else 204
 
 @app.route('/random_recipe')
 def random_recipe():
